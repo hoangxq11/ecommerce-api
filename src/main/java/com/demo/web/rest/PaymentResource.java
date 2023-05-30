e985446dcff27392c95bca205be09d681ecfa9fe
hoangxq <holmes.arther@gmail.com>
2023-05-30 23:00:17 +0700

    Update VNPAY utils

---
+++ src/main/java/com/demo/web/rest/PaymentResource.java
@@ -0,0 +1,99 @@
+package com.demo.web.rest;
+
+import com.demo.config.VnPayConfig;
+import com.demo.web.dto.PaymentResDto;
+import com.demo.web.dto.response.utils.ResponseUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/payment")
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class PaymentResource {
+
+    @GetMapping("/create-payment/{amount}")
+    public ResponseEntity<?> createPayment(@PathVariable Integer amount) throws UnsupportedEncodingException {
+
+//        String orderType = req.getParameter("ordertype");
+//        long amount = Integer.parseInt(req.getParameter("amount"))*100;
+//        String bankCode = req.getParameter("bankCode");
+
+        amount*=100;
+
+        String vnp_TxnRef = VnPayConfig.getRandomNumber(8);
+//        String vnp_IpAddr = VnPayConfig.getIpAddress(req);
+        String vnp_TmnCode = VnPayConfig.vnp_TmnCode;
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", VnPayConfig.vnp_Version);
+        vnp_Params.put("vnp_Command", VnPayConfig.vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+        vnp_Params.put("vnp_BankCode", "NCB");
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_Locale", "vn");
+        vnp_Params.put("vnp_ReturnUrl", VnPayConfig.vnp_Returnurl);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = VnPayConfig.hmacSHA512(VnPayConfig.vnp_HashSecret, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = VnPayConfig.vnp_PayUrl + "?" + queryUrl;
+
+        PaymentResDto paymentResDto = new PaymentResDto();
+        paymentResDto.setStatus("DONE");
+        paymentResDto.setMessage("Successfully");
+        paymentResDto.setURL(paymentUrl);
+
+        return ResponseUtils.ok(paymentResDto);
+    }
+
+    @RequestMapping("/transaction")
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("payment-success");
+        return modelAndView;
+    }
+}
